15d99d4f6dea4b80578b1e8bf0680b23f9e61779
monalisha99 <bmobalisha1@gmail.com>
2025-07-26 00:27:51 +0530

    modified text editor

--- text-editor.py
+++ text-editor.py
@@ -1,15 +1,22 @@
-
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import font
 
+
+
+
 # define save task function
 def save_task():
 	# retrieve texts from the text box using get function
 	texts = text_box.get("1.0", tk.END)
-	with open("notebook.txt", "w") as file:
-		file.write(texts)
+	if len(texts)!=0 or texts!=None:
+
+		with open("notebook.txt", "w") as file:
+			file.write(texts)
+
+		messagebox.showinfo("saved")
+
 
 
 def open_file():
@@ -46,22 +53,22 @@ root.title("Text Editor")
 root.geometry("690x320")
 
 # textbox resize
-#(When resize the window, the Text box will grow/shrink with it)
-root.grid_rowconfigure(0, weight=1)
+# (When resize the window, the Text box will grow/shrink with it)
+root.grid_rowconfigure(1, weight=1)  # Only row 1 is resizable
 root.grid_columnconfigure(0, weight=1)
 
 # add widgets
 # define Textbox and place it
-text_box = tk.Text(root, font="Times, 14")
-text_box.grid(row=0, column=0, sticky='nsew', padx=15, pady=15)
+text_box = tk.Text(root, font=("Courier New", 16))
+text_box.grid(row=1, column=0, columnspan=2, padx=15, pady=3, sticky='nsew')
 
 # define save button and place it
-save_button = tk.Button(root, text='Save', width=8, height=1, bg='lightblue', fg= 'black', activebackground='darkblue', activeforeground='white', command=save_task)
-save_button.grid(row=0, column=1, sticky='ne', padx=10, pady=10)
+save_button = tk.Button(root, text='Save', width=8, height=2, bg='lightblue', fg='black', activebackground='darkblue', activeforeground='white', command=save_task)
+save_button.grid(row=0, column=0, sticky='nw', padx=(10,2), pady=3)
 
 # define open button and place it
-open_button = tk.Button(root, text='Open',width=8, height=1, bg='lightblue', fg= 'black', activebackground='darkblue', activeforeground='white', command=open_file)
-open_button.grid(row=0, column=2, sticky='ne', padx=10, pady=10)
+open_button = tk.Button(root, text='Open', width=8, height=2, bg='lightblue', fg='black', activebackground='darkblue', activeforeground='white', command=open_file)
+open_button.grid(row=0, column=1, sticky='nw', padx=(2,10), pady=3)
 
 root.mainloop()
 
